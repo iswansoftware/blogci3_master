17ee4efb2227080b56e98b91e17e616fddb070c8
iswansoftware <iswansoftware@gmail.com>
2022-06-25 01:23:35 +0700

    Opreks UI

--- application/controllers/Utama.php
+++ application/controllers/Utama.php
@@ -4,6 +4,73 @@ class Utama extends CI_Controller
 {
 	public function index()
 	{
-		$this->template->load(template() . '/template', template() . '/view_home');
+		$this->load->view(template() . '/_header');
+		$this->load->view(template() . '/_navbar');
+		$this->load->view(template() . '/_slider');
+		$this->load->view(template() . '/_landingpage');
+		$this->load->view(template() . '/_footer');
+	}
+
+	public function about()
+	{
+		$this->load->view(template() . '/_header');
+		$this->load->view(template() . '/_navbar');
+		$this->load->view(template() . '/_slider');
+		$this->load->view(template() . '/_about');
+		$this->load->view(template() . '/_footer');
+	}
+
+	public function contact()
+	{
+		$this->load->view(template() . '/_header');
+		$this->load->view(template() . '/_navbar');
+		$this->load->view(template() . '/_slider');
+		$this->load->view(template() . '/contact');
+		$this->load->view(template() . '/_footer');
+	}
+
+	public function feature()
+	{
+		$this->load->view(template() . '/_header');
+		$this->load->view(template() . '/_navbar');
+		$this->load->view(template() . '/_slider');
+		$this->load->view(template() . '/_feature');
+		$this->load->view(template() . '/_footer');
+	}
+
+	public function project()
+	{
+		$this->load->view(template() . '/_header');
+		$this->load->view(template() . '/_navbar');
+		$this->load->view(template() . '/_slider');
+		$this->load->view(template() . '/_project');
+		$this->load->view(template() . '/_footer');
+	}
+
+	public function service()
+	{
+		$this->load->view(template() . '/_header');
+		$this->load->view(template() . '/_navbar');
+		$this->load->view(template() . '/_slider');
+		$this->load->view(template() . '/_service');
+		$this->load->view(template() . '/_footer');
+	}
+
+	public function team()
+	{
+		$this->load->view(template() . '/_header');
+		$this->load->view(template() . '/_navbar');
+		$this->load->view(template() . '/_slider');
+		$this->load->view(template() . '/_team');
+		$this->load->view(template() . '/_footer');
+	}
+
+	public function testimonial()
+	{
+		$this->load->view(template() . '/_header');
+		$this->load->view(template() . '/_navbar');
+		$this->load->view(template() . '/_slider');
+		$this->load->view(template() . '/_testimonial');
+		$this->load->view(template() . '/_footer');
 	}
 }
